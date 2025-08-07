062323b96b23ceb481feb801ffd172389602b2fe
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2025-08-07 21:44:16 +0530

    Add files via upload

---
+++ missing number.py
@@ -0,0 +1,11 @@
+def func(nums):
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+
+# Example usage
+if __name__ == "__main__":
+    nums = [3, 0, 1]
+    result = func(nums)
+    print("Missing number is:", result)
