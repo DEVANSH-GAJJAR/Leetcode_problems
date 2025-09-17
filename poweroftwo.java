3c433e6d28e30044892fb0ab9791e5b77939bf0c
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2025-09-17 19:15:20 +0530

    Add files via upload

---
+++ poweroftwo.java
@@ -0,0 +1,26 @@
+public class poweroftwo
+{
+    public static boolean myFunc(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        while (n % 2 ==0)
+        {
+            n = n / 2;
+        }
+
+        return n == 1;
+
+    }
+
+    public static void main(String[] args) {
+        
+        poweroftwo solution = new poweroftwo();
+        int param1 = 16;
+        boolean result = solution.myFunc(param1);
+        System.out.println(result);
+
+    }
+}
