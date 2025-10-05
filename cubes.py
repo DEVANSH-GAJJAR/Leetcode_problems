d6a00db031151bea3c4ff6cd796d06b08e18233b
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2025-10-05 16:49:41 +0530

    Add files via upload

---
+++ cubes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def my_func(self, n):  
+        if n < 0:
+            n = abs(n)
+
+        root = round(n**(1/3))
+        return root**3 == n
+
+# Example usage
+if __name__ == "__main__":
+    solution = Solution()
+    param1 = 8
+    result = solution.my_func(param1)
+    print(result)
