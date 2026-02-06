a5ec3825fc93eeb673efdc14c9d1e8967b5e7abb
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2026-02-06 12:46:37 +0530

    Update Two_Sum.py

--- Two_Sum.py
+++ Two_Sum.py
@@ -1,8 +1,14 @@
-def twoSum(nums, target):
-    seen = {}  # value -> index
+class Solution:
     
-    for i, num in enumerate(nums):
-        rem = target - num
-        if rem in seen:
-            return [seen[rem], i]
-        seen[num] = i
+     def twoSum(nums, target):
+     seen = {}  
+    
+     for i, num in enumerate(nums):
+        
+         rem = target - num
+         if rem in seen:
+             return [seen[rem], i]
+         seen[num] = i
+#example usage 
+ if __name__ == "__main__":
+     solution = Solution()
