2d06f02fdb0bcc550865324078f80e8a601005ef
DEVANSH GAJJAR <162860692+DEVANSH-GAJJAR@users.noreply.github.com>
2026-07-11 21:33:52 +0530

    Create MeanOfAnArray.py

---
+++ MeanOfAnArray.py
@@ -0,0 +1,14 @@
+## MEAN OF AN ARRAY 
+def findmean(arr):
+
+  sum = 0 
+  n = len(arr)
+  for i in range(n):
+    sum += arr[i]
+  ans = sum // n
+
+return ans 
+#####
+if __name__ == "__main__":
+  arr = [1,3,5,4,2,5]
+print(findmean(arr))
